d18a39e32322c8c8b2e28c11752cfc02a33dd9eb
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-09-09 10:55:10 +0200

    Add Constructors to Rectangle, Circle, Triangle

--- Circle.java
+++ Circle.java
@@ -2,6 +2,10 @@ public class Circle  extends Shape {
 
     private double radius;
 
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
     public double getRadius() {
         return radius;
     }
--- Triangle.java
+++ Triangle.java
@@ -3,6 +3,11 @@ public class Triangle extends Shape {
     private double base;
     private double height;
 
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
     public double getBase() {
         return base;
     }
