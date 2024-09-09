8a0796575ca1cda0a59dabae38ec436c9dff954a
bruch-alex <alex.bruch@tutanota.com>
2024-09-09 11:06:24 +0200

    add solution

--- Main.java
+++ Main.java
@@ -1,3 +1,24 @@
-public class Main{
+public class Main {
+    public static void main(String[] args) {
+        // Upcasting
+        Shape triangleShape = new Triangle(2, 3);
+        Shape circleShape = new Circle(5);
+        Shape rectangleShape = new Rectangle(5, 10);
+
+//        System.out.println(circleS.radius); // can't access
+
+        System.out.println(triangleShape.calculateArea());
+        System.out.println(circleShape.calculateArea());
+        System.out.println(rectangleShape.calculateArea());
+
+        //Down-casting
+        Circle circle = (Circle) circleShape;
+        Triangle triangle = (Triangle) triangleShape;
+        Rectangle rectangle = (Rectangle) rectangleShape;
+
+        System.out.println(circle.radius);
+        System.out.println(triangle.base);
+        System.out.println(rectangle.width);
+    }
 
 }
---
+++ Shape.java
@@ -0,0 +1,49 @@
+public abstract class Shape {
+    String name;
+    String color;
+
+    abstract double calculateArea();
+}
+
+class Circle extends Shape {
+    double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle extends Shape {
+    double width;
+    double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    double calculateArea() {
+        return width * height;
+    }
+}
+
+class Triangle extends Shape {
+    double base;
+    double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    double calculateArea() {
+        return base * height / 2;
+    }
+}
